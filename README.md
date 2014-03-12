e78ec8615da7ec3379a0cbad6afc183e796df4fd
Vladimir Mironov <vladimir.i.mironov@gmail.com>
2014-03-12 14:23:07 +0000

    first

---
+++ README.md
@@ -0,0 +1,15 @@
+Description
+===========
+Recipe should download, unpack and put applicationjars to Jboss deploy folder. Then restart Jboss.
+http://www.cumulogic.com/download/Apps/testweb.zip
+
+
+Requirements
+============
+
+Attributes
+==========
+
+Usage
+=====
+
---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "EPAM Systems"
+maintainer_email "vladimir.i.mironov@gmail.com"
+license          "All rights reserved"
+description      "Installs/Configures my-jboss-app"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
